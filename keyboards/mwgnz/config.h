21fe8b10211de2dab8de5cc946000e25ad6913e8
MWGNZ <github.com@t.o.omuch.info>
2024-05-03 21:51:07 +1200

    holy shit it builds again

--- keyboards/mwgnz/config.h
+++ keyboards/mwgnz/config.h
@@ -3,15 +3,6 @@
 
 #pragma once
 
-#include "config_common.h"
-
-/* USB Device descriptor parameter */
-#define VENDOR_ID    0xFEED
-#define PRODUCT_ID   0xDEAD
-#define DEVICE_VER   0xBEEF
-#define MANUFACTURER MWGNZ
-#define PRODUCT      mwgnz
-
 /* key matrix size */
 #define MATRIX_ROWS 16
 #define MATRIX_COLS 9
@@ -62,10 +53,6 @@
 #define NO_ACTION_LAYER
 #define NO_ACTION_TAPPING
 #define NO_ACTION_ONESHOT
-
-/* disable these deprecated features by default */
-#define NO_ACTION_MACRO
-#define NO_ACTION_FUNCTION
 
 /* Bootmagic Lite key configuration */
 #define BOOTMAGIC_LITE_ROW 0
